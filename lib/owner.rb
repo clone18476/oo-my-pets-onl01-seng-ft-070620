4ef1c86b862ea6ac1f6b9abee2acbf6d8f85866c
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-15 22:36:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -24,43 +24,5 @@ class Owner
     "I am a #{self.species}."
   end
 
-  def buy_cat(name)     # can buy a cat that is an instance of the Cat class
-    Cat.new(name, self)    # knows about its cats
-  end
 
-  def buy_dog(name)     # can buy a dog that is an instance of the Dog class
-    Dog.new(name, self)   # knows about its dogs 
-  end
-
-  def walk_dogs    # walks the dogs which makes the dogs' moods happy
-   self.dogs.each {|dog| dog.mood = "happy"}
-  end
-
-  def feed_cats   # feeds the cats which makes the cats' moods happy
-    self.cats.each {|cat| cat.mood = "happy"}
-  end
-
-  def sell_pets    # can sell all its pets, which makes them nervous
-    self.cats.each do |cat|
-      self.cats.delete(cat)
-      cat.owner = nil
-      cat.mood = "nervous"
-    end
-    self.dogs.each do |dog|
-      self.dogs.delete(dog)
-      dog.owner = nil
-      dog.mood = "nervous"
-    end
-  end
-
-  def list_pets    # can list off its pets 
-    number_of_dogs = self.dogs.count
-    number_of_cats = self.cats.count
-
-    "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
-  end
-
-  def self.reset_all       # .reset_all can reset the owners that have been created
-    self.all.clear
-  end
 end
