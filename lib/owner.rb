c59ffbd18e3d3b377d1f97af9b0325daf57d96a2
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-15 22:28:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,12 @@
 class Owner
   
+  attr_accessor 
+  attr_reader :species, :name
+  
+  
   def initialize(name)  #initializes with a name 
-    @name = name
+    @name = name        # cannot change owners name 
+    @species = "human"  #initializes with species set to human
   end 
   
   
