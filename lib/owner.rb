e3f450d00560710c5280e8f7d0debc4f0e78d53b
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-15 22:33:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,17 +3,26 @@ class Owner
   attr_accessor 
   attr_reader :species, :name     # cannot change name or species
   
+  @@all = []
+  
   
   def initialize(name)  #initializes with a name 
     @name = name        # cannot change owners name 
     @species = "human"  #initializes with species set to human
+    @@all << self     # .all returns all instances of Owner that have been created 
   end 
   
   def say_species   #can say it's species 
     "I am a human."
   end 
   
-  
-  
+  def self.all   #.all returns all instances of Owner that have been created
+    @@all
+  end 
+
+
+  def self.count   # .count returns the number of owners that have been created
+    
+  end  
   
 end 
